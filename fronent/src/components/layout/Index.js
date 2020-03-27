9e410a393015bce4537238e052d928d9273596cf
YasserYka <yasseryka@gmail.com>
2020-03-27 15:48:54 +0300

    partial refactoring

--- fronent/src/components/layout/Index.js
+++ fronent/src/components/layout/Index.js
@@ -1,9 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component, useRef } from 'react';
 import Watch from '../Watch';
 
 class Index extends Component {
 
-    state = {
+    constructor(){
+        super();
+
+        this.state = {
+            showForm: false
+        }
+        this.showFormComponent = this.showFormComponent.bind(this);
+    }
+
+    /*state = {
         url: "ws://localhost:8080",
     }
     
@@ -21,11 +30,47 @@ class Index extends Component {
         }
     }
 
+    submitCreateRoom = event => {
+        if(event)
+            event.preventDefault();
+
+        this.socket.send(JSON.stringify({
+            event: "room",
+            action: "create",
+            roomId: this.refs.roomId.value,
+            username: this.refs.username.value
+        }));
+    }*/
+
+    showFormComponent = () => this.setState({showForm: true});
+
+
+
     render() {
         return (
             <React.Fragment>
-                <Watch socket={this.socket} />
+                {this.state.showForm ? 
+                        <form>
+                        <input type="text" ref="username" placeholder="You Username" required />
+                        <input type="text" ref="roomId" placeholder="Room Id" required />
+                        <input type="submit" value="Submit" />
+                        </form> 
+                        : null
+                }
+                <div className="modal-body">
+                    <button onClick={this.showFormComponent} value="create" className="btn btn-info btn-lg btn-block">Create Room</button>
+                    <button onClick={this.showFormComponent} value="join" className="btn btn-secondary btn-lg btn-block">Join Room</button>
+                </div>
             </React.Fragment>
+            /*
+            <React.Fragment>
+                <form onSubmit={this.submitCreateRoom}>
+                    <input type="text" ref="username" placeholder="You Username" required />
+                    <input type="text" ref="roomId" placeholder="Room Id" required />
+                    <input type="submit" value="Submit" />
+                </form>
+                <Watch socket={this.socket} />
+            </React.Fragment>*/
         )
     }
 }
